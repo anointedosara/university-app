b8d8da52c061a109d8b8e2fbe186ab207278a5ee
anointed osara <anointedosara@gmail.com>
2022-08-23 18:04:00 +0530

    first commit

--- src/Component/Countries.js
+++ src/Component/Countries.js
@@ -16,7 +16,7 @@ function Countries() {
     }, [])
   return (
     <div className='countries'>
-      Here is a list of all countries to pick from.
+      <h1>Here is a list of all countries to pick from.</h1>
       {
         data.map((item, i) => <p key={i}><Link to={`/country/${item.name.common}`}>{item.name.common}</Link></p>)
       }
